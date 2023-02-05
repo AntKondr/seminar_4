aba04c9343550f70dfa8ec6c94fc853ac50b5d7b
Anton_Kondratev <elkondro@gmail.com>
2023-02-06 04:54:29 +0700

    task_27 solution

--- task_27/Program.cs
+++ task_27/Program.cs
@@ -1,2 +1,13 @@
 ﻿Console.Clear();
 Console.Write("Seminar_4 task_27\n\n");
+
+int digits_sum(string num){
+    int sum = 0;
+    for (int i = 0; i < num.Length; i++){
+        sum += Convert.ToInt32($"{num[i]}");
+    }
+    return sum;
+}
+
+Console.Write("input number: ");
+Console.Write($"sum of digits of number = {digits_sum(Console.ReadLine())}");
