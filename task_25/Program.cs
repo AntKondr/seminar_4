02b8984ccb3d975a1d63f3df793b0f9c8e3272d6
Anton_Kondratev <elkondro@gmail.com>
2023-02-06 04:35:59 +0700

    task_25 solution

--- task_25/Program.cs
+++ task_25/Program.cs
@@ -1,2 +1,20 @@
 ﻿Console.Clear();
 Console.Write("Seminar_4 task_25\n\n");
+
+double pow(double num, int rate){
+    double res = 1;
+    int count = 0;
+    while (count < rate){
+        res = res * num;
+        count += 1;
+    }
+    return res;
+}
+
+Console.Write("input number: ");
+double number = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("input power: ");
+int power = Convert.ToInt32(Console.ReadLine());
+
+Console.Write($"{number} in {power} power = {pow(number, power)}");
